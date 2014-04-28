2b41e4e3e8df62ca7689103d1a04d231f49e7e08
Viktor Horvath <ric.flair.wcw@gmail.com>
2014-04-28 10:46:49 +0100

    1

---
+++ utility-web/src/main/java/com/googlecode/happymarvin/webutility/beans/InstructionCreateInputBean.java
@@ -0,0 +1,13 @@
+package com.googlecode.happymarvin.webutility.beans;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class InstructionCreateInputBean {
+
+	private Map<String, Set<String>> typeTemplates;
+	private List<> textFields;
+	
+}
